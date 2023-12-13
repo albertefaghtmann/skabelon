e8b2f1142fc577680395c16ab2c71d177976a568
Line Ploger <lilleploger@gmail.com>
2023-12-13 11:26:28 +0100

    footer og form

---
+++ src/scripts/form.js
@@ -0,0 +1,12 @@
+/*const form = document.querySelector("#form");
+const form2 = document.querySelector("#form2");
+const form3 = document.querySelector("#form3");
+const Primarybutton = document.querySelector(".primarybutton");
+
+form.addEventListener("click", function () {
+  form.classList.toggle("close");
+  form2.classList.toggle("overlay");
+});
+
+Primarybutton.addEventListener("click", alert("hej"));
+*/
